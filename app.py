17d256ab8bf55ab0b60db3caa1c24887e81bde33
dbdyadbdya <dbdya03@gmail.com>
2026-09-03 17:30:19 +0900

    chore: Flask 기초 폴더 구조(templates, static) 설정 및, gitignore 추가

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 메인 페이지 연동
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
